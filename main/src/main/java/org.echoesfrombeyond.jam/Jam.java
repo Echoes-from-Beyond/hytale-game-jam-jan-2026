a84ecdd2a951f4f991e108a3f1925c07288e7cbc
VegetalDev <chemky2000@gmail.com>
2026-01-31 03:01:15 -0800

    feat: colonists, food, water, scrap

--- main/src/main/java/org.echoesfrombeyond.jam/Jam.java
+++ main/src/main/java/org.echoesfrombeyond.jam/Jam.java
@@ -12,18 +12,39 @@ public class Jam implements Resource<ChunkStore> {
   public static final BuilderCodec<Jam> CODEC =
       BuilderCodec.builder(Jam.class, Jam::new)
           .append(
-              new KeyedCodec<>("ClickCounter", Codec.INTEGER),
-              (self, value) -> self.clickCounter = value,
-              (self) -> self.clickCounter)
+              new KeyedCodec<>("Colonists", Codec.INTEGER),
+              (self, value) -> self.colonists = value,
+              (self) -> self.colonists)
+          .add()
+          .append(
+              new KeyedCodec<>("Food", Codec.INTEGER),
+              (self, value) -> self.food = value,
+              (self) -> self.food)
+          .add()
+          .append(
+              new KeyedCodec<>("Water", Codec.INTEGER),
+              (self, value) -> self.water = value,
+              (self) -> self.water)
+          .add()
+          .append(
+              new KeyedCodec<>("Scrap", Codec.INTEGER),
+              (self, value) -> self.scrap = value,
+              (self) -> self.scrap)
           .add()
           .build();
 
-  public int clickCounter;
+  public int colonists;
+  public int food;
+  public int water;
+  public int scrap;
 
   public Jam() {}
 
   public Jam(Jam other) {
-    this.clickCounter = other.clickCounter;
+    this.colonists = other.colonists;
+    this.food = other.food;
+    this.water = other.water;
+    this.scrap = other.scrap;
   }
 
   @Override
