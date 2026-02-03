b47ee5213f3e90f158a7bbcc36c62da932010d12
Cobrinthine <cobrinthine.amaranthine@gmail.com>
2026-02-03 01:55:04 +0000

    feat: proper join popup

--- main/src/main/java/org/echoesfrombeyond/jam/JoinUI.java
+++ main/src/main/java/org/echoesfrombeyond/jam/JoinUI.java
@@ -21,7 +21,33 @@ public class JoinUI extends CustomUIPage {
       @NotNull UICommandBuilder commandBuilder,
       @NotNull UIEventBuilder eventBuilder,
       @NotNull Store<EntityStore> store) {
+    String foreword = """
+                      The fallen magitech metropolis of Crucible looms over you.
+                      Your first foray into the city ended with your entire squad decimated by its
+                      fiery automatons, somehow still functional after centuries of neglect.
+                      Regroup on the outskirts and fend off its overactive defense systems...
+                      maybe that radio tower will help if you manage to rewire it?
+                      """;
+
+    // sigh, we need noesis so bad
+    String joinText = """
+            Scrapvengers is a simple tech demo of a colony simulator made in Hytale.
+            Manage your resources and defend the heart of your base (the radio tower)
+            against intruders. Repair the tower to win; neglect it too much and the
+            consequences are obvious.
+            This mod is played almost entirely with your mouse. Use the menus in your
+            hotbar to place buildings and to advance the day (i.e. gather your
+            resources). Be careful, because your enemies strike in meantime.
+
+            Avoid loading another mod alongside this one. Scrapvengers is meant to be
+            standalone - it changes enough of the game that things are bound to go
+            wrong otherwise.
+            We hope you enjoy our dumb little experiment :)
+            - Cobrinthine & Vegetal
+            
+            (Most menus can be closed with ESC)""";
     commandBuilder.append("Join_Popup.ui");
-    commandBuilder.set("#JoinText.Text", "test");
+    commandBuilder.set("#Foreword.Text", foreword);
+    commandBuilder.set("#JoinText.Text", joinText);
   }
 }
